fb1affd89b4af427643ee1546b90cd5dc9dff2ec
Buddhi Pathirane <bpathirane@definitivehc.com>
2019-02-16 22:09:20 +0530

    Fixed lint errors

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const fp = require('fastify-plugin')
 const jsend = require('jsend')
 
 module.exports = fp(function (fastify, options, next) {
-    console.log('Registering jsend-fastify plugin')
-    fastify.decorateReply('jsend', function (payload) {
-            var modifiedPayload = jsend.success(payload)
-            this.send(modifiedPayload)
-        })
-    next()
+  const success = function (payload) {
+    var modifiedPayload = jsend.success(payload)
+    this.send(modifiedPayload)
+  }
+
+  fastify.decorateReply('jsend', success)
+
+  next()
 })
--- test.js
+++ test.js
@@ -4,26 +4,30 @@ const test = require('tap').test
 const Fastify = require('fastify')
 const jf = require('.') // jsend-fastify
 
-test('it should add default jsend method and it should be success', (t) => {
+test('it should decorate reply with jsend utility methods', (tOut) => {
+  tOut.plan(1)
+
+  tOut.test('it should add default jsend method and it should be success', (t) => {
     const fastify = Fastify()
 
     const acct = { id: 1234 }
-    const payload = { account: acct}
+    const payload = { account: acct }
 
     fastify.register(jf)
 
     fastify.get('/', (request, reply) => {
-        reply.jsend(payload)
+      reply.jsend(payload)
     })
 
     fastify.inject({
-        method: 'GET',
-        url: '/'
+      method: 'GET',
+      url: '/'
     }, (err, res) => {
-        t.error(err)
-        const respPayload = JSON.parse(res.body)
-        t.equals(respPayload.status, 'success') 
-        t.looseEquals(respPayload.data, payload)
-        t.end()
+      t.error(err)
+      const respPayload = JSON.parse(res.body)
+      t.equals(respPayload.status, 'success')
+      t.looseEquals(respPayload.data, payload)
+      t.end()
     })
+  })
 })
