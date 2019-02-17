bb72bbd70a71a76dc2638416c3f75648739b1c85
Buddhi Pathirane <bpathirane@definitivehc.com>
2019-02-17 09:35:00 +0530

    Adding the jsendFail decorator

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const fp = require('fastify-plugin')
 const jsend = require('jsend')
 
 module.exports = fp(function (fastify, options, next) {
-  const success = function (payload) {
+  function success (payload) {
     var modifiedPayload = jsend.success(payload)
     this.send(modifiedPayload)
   }
 
+  function fail(payload) {
+      this.send(jsend.fail(payload))
+  }
+
   fastify.decorateReply('jsend', success)
+  fastify.decorateReply('jsendSuccess', success)
+  fastify.decorateReply('jsendFail', fail)
 
   next()
 })
--- test.js
+++ test.js
@@ -5,15 +5,14 @@ const Fastify = require('fastify')
 const jf = require('.') // jsend-fastify
 
 test('it should decorate reply with jsend utility methods', (tOut) => {
-  tOut.plan(1)
+  tOut.plan(4)
+
+  const acct = { id: 1234 }
 
   tOut.test('it should add default jsend method and it should be success', (t) => {
     const fastify = Fastify()
-
-    const acct = { id: 1234 }
-    const payload = { account: acct }
-
     fastify.register(jf)
+    const payload = { account: acct }
 
     fastify.get('/', (request, reply) => {
       reply.jsend(payload)
@@ -25,7 +24,70 @@ test('it should decorate reply with jsend utility methods', (tOut) => {
     }, (err, res) => {
       t.error(err)
       const respPayload = JSON.parse(res.body)
+      t.strictEquals(res.statusCode, 200)
+      t.match(res.get)
+      t.strictEquals(respPayload.status, 'success')
+      t.looseEquals(respPayload.data, payload)
+      t.end()
+    })
+  })
+
+  tOut.test('should add jsendSuccess', (t) => {
+    const fastify = Fastify()
+    fastify.register(jf)
+    const payload = { account: acct }
+
+    fastify.get('/', (request, reply) => {
+      reply.jsendSuccess(payload)
+    })
+
+    fastify.inject({
+      method: 'GET',
+      url: '/'
+    }, (err, res) => {
+      t.error(err)
+      const respPayload = JSON.parse(res.body)
       t.equals(respPayload.status, 'success')
+      t.looseEquals(respPayload.data, payload)
+      t.end()
+    })
+  })
+
+  tOut.test('should add jsendFail', (t) => {
+    const fastify = Fastify()
+    fastify.register(jf)
+    const payload = { property: 'Failure'}
+    fastify.get('/', (request, reply) => {
+      reply.jsendFail(payload)
+    })
+
+    fastify.inject({
+      method: 'GET',
+      url: '/'
+    }, (err, res) => {
+      t.error(err)
+      const respPayload = JSON.parse(res.body)
+      t.equals(respPayload.status, 'fail')
+      t.looseEquals(respPayload.data, payload)
+      t.end()
+    })
+  })
+
+  tOut.test('should add jsendError', (t) => {
+    const fastify = Fastify()
+    fastify.register(jf)
+    const payload = { property: 'Failure'}
+    fastify.get('/', (request, reply) => {
+      reply.jsendFail(payload)
+    })
+
+    fastify.inject({
+      method: 'GET',
+      url: '/'
+    }, (err, res) => {
+      t.error(err)
+      const respPayload = JSON.parse(res.body)
+      t.equals(respPayload.status, 'fail')
       t.looseEquals(respPayload.data, payload)
       t.end()
     })
